bce6caed97dbba38e8fa1c36a74d97b1220ae848
Irma Mejia <cursosdecomputacion.net@gmail.com>
2021-09-14 17:21:40 -0700

    Agregando Frase Motivadora

--- Ventas/Ventas/Form1.cs
+++ Ventas/Ventas/Form1.cs
@@ -19,7 +19,7 @@ namespace Ventas
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hola Mundo");
+            MessageBox.Show("Si lo puedes soñar, lo puedes programar");
         }
     }
 }
